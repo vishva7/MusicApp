09673a2b4da82a11812095996a26d3eaa131dddd
vishva7 <vishvamehta301@gmail.com>
2023-10-18 16:02:38 +0530

    add navbar,sidebar,prelogin

--- src/PreLogin.js
+++ src/PreLogin.js
@@ -1,10 +1,19 @@
 import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import Card from "./card";
 function Prelogin() {
-    return(
-      <div>
-    <Sidebar/>
+  return (
+    <div>
+      <div className="w-64">
+        <Sidebar />
+      </div>
+      <div className="w-[calc(100vw-64)]">
+        <Navbar />
+
+        <Card />
+      </div>
     </div>
-    );
-  }
-  
-  export default Prelogin;
+  );
+}
+
+export default Prelogin;
