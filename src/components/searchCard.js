2a880f5b26ea44f6ed5073a7ea9a02059ace1598
vishva7 <vishvamehta301@gmail.com>
2023-10-23 13:02:48 +0530

    made search page

--- src/components/searchCard.js
+++ src/components/searchCard.js
@@ -3,7 +3,21 @@ const SearchCard = ({search}) => {
   return (
     <div className="grid grid-cols-7 gap-2">
     {search.map((SE) => (
-    <div className="w-40 h-40 bg-[#27856A] rounded-lg pl-3 pt-3">
+    <div
+    //   className={`w-40 h-40 bg-${SE.color} rounded-lg pl-3 pt-3`}
+    style={{
+      width: "160px",
+      height: "160px",
+      backgroundColor: SE.color,
+      borderRadius: "8px",
+      paddingLeft: "12px",
+      paddingTop: "12px",
+      fontFamily: 'var(--font-family,CircularSpTitle,CircularSpTitle-Tall,CircularSp-Arab,CircularSp-Hebr,CircularSp-Cyrl,CircularSp-Grek,CircularSp-Deva,var(--fallback-fonts,sans-serif))',
+      fontSize:'22px',
+      color:'white',
+    }}
+    key={SE.id}
+  >
       
         <b>{SE.title}</b>
       
--- src/routes/explore.js
+++ src/routes/explore.js
@@ -4,11 +4,35 @@ import SearchCard from "../components/searchCard";
 import {useState} from 'react'; 
 
 function Explore() {
-    const [search,setSearch]=useState([{title:"Today's Top Hits",id:1,img:"/album1.webp"},
-  {title:"Rap Caviar",id:2},
-  {title:"All Out 2010s",id:3},
-  {title:"Rock Classics",id:4},
-  {title:"Chill Hits",id:4}
+    const [search,setSearch]=useState([{title:"Podcasts",id:1, color:'#27856A'},
+  {title:"Live Events",id:2, color:'#283EA3'},
+  {title:"Made For You",id:3, color:'#1E3264'},
+  {title:"New Releases",id:4, color:'#8D67AB'},
+  {title:"Hindi",id:5, color:'#E8115B'},
+  {title:"Punjabi",id:6, color:'#8D67AB'},
+  {title:"Tamil",id:7, color:'#7358FF'},
+  {title:"Telugu",id:8, color:'#B49BC8'},
+  {title:"Charts",id:9, color:'#F037A5'},
+  {title:"Pop",id:10, color:'#9CF0E1'},
+  {title:"Indie",id:11, color:'#E1118B'},
+  {title:"Trending",id:12, color:'#D7F27D'},
+  {title:"Love",id:13, color:'#283EA3'},
+  {title:"Discover",id:14, color:'#777777'},
+  {title:"Mood",id:15, color:'#DC148C'},
+  {title:"Party",id:16, color:'#283EA3'},
+  {title:"Devotional",id:17, color:'#1E3264'},
+  {title:"Decades",id:18, color:'#B49BC8'},
+  {title:"Hip-Hop",id:19, color:'#E1118B'},
+  {title:"Electronic",id:20, color:'#8D67AB'},
+  {title:"Student",id:21, color:'#D7F27D'},
+  {title:"Chill",id:22, color:'#E8115B'},
+  {title:"Gaming",id:23, color:'#8D67AB'},
+  {title:"K-Pop",id:24, color:'#9CF0E1'},
+  {title:"Workout",id:25, color:'#7358FF'},
+  {title:"Radar",id:26, color:'#777777'},
+  {title:"Equal",id:27, color:'#D7F27D'},
+  {title:"Rock",id:28, color:'#F037A5'}
+
   ])
     return ( 
         <div className="encl">
