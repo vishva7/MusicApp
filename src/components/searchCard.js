00e5fe3715a64382fe6c67953af74d486b27de78
vishva7 <vishvamehta301@gmail.com>
2023-10-26 16:36:59 +0530

    modified

--- src/components/searchCard.js
+++ src/components/searchCard.js
@@ -1,7 +1,7 @@
 // import React, { useRef, useState } from "react";
 const SearchCard = ({ search }) => {
   return (
-    <div className="grid grid-cols-7 gap-2">
+    <div className="grid grid-cols-2 md:grid-cols-7 gap-2">
       {search.map((SE) => (
         <div
           //   className={`w-40 h-40 bg-${SE.color} rounded-lg pl-3 pt-3`}
