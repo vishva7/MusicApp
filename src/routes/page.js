d0519fd1e07c86f3b0e87a90cbaf74d02fc35148
Vijju <vijjuhavaldar@gmail.com>
2023-11-02 11:41:14 +0530

    modifiedlogin

---
+++ src/routes/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageWithBlackBox from './loginpage';
+
+function App() {
+  return (
+    <div className="App">
+      <PageWithBlackBox />
+    </div>
+  );
+}
+
+export default App;
