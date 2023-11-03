39401177af64b10e90171375aedfd101534a823b
vishva7 <vishvamehta301@gmail.com>
2023-11-03 08:36:37 +0530

    modified

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -32,19 +32,24 @@ const Sidebar = () => {
       >
         <div class="h-full px-3 py-4 overflow-y-auto bg-black">
           <ul class="space-y-2 font-medium">
-            <li>
-              <svg
-                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 21"
-              >
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-              </svg>
-              <span class="ml-3 text-white ">Music Next</span>
+            <li className="px-0">
+              <a href="https://flowbite.com/" class="flex-shrink-0 w-0 h-0 ">
+                
+
+              <span class="flex ml-0 whitespace-nowrap text-white ">
+              <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  class="h-8 ml-0  bottom-16"
+                  alt="FlowBite Logo"
+                />
+                <div className="ml-4 text-lg">
+            
+                Music Next
+                </div>
+              </span>
+              </a>
             </li>
+            
             <li>
               <Link
                 to="/Prelogin"
