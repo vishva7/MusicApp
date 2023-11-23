7bb21eb1c381ee46870b9fcbb05d5fc5331c847d
Viona <vionasequeira9@gmail.com>
2023-11-23 14:01:16 +0530

    new

--- src/routes/playlist1.js
+++ src/routes/playlist1.js
@@ -4,9 +4,9 @@ const Playlist1 = () => {
     
     const [song, setSong] = useState([
         { title: "Driver's License", album: "Sour", date: "13 october",src:"Music/Olivia Rodrigo drivers license (Lyrics).mp3" ,time: "3:11", id: 1, img: "/drive.jpg" },{ title: "Wildest Dreams", album: "1989", date: "13 october",src:"/Taylor Swift - Wildest Dreams (Taylor’s Version) (Official Audio)_160k.mp3" ,time: "3:11", id: 1, img: "/tsrep.png" },
-        { title: "Jar of hearts", album: "Single", date: "13 october",src:"Music/Christina Perri - Jar of Hearts.mp3",time: "3:11", id: 2, img: "/tsrep.png" },
+        { title: "Jar of hearts", album: "Single", date: "13 october",src:"Music/Christina Perri - Jar of Hearts.mp3",time: "3:11", id: 2, img: "jar of hearts.jpg" },
         { title: "We Belong", album: "Single", date: "13 october", src:"Music/Dove Cameron - We Belong (Official Lyric Video).mp3",time: "3:11", id: 3, img: "/tsrep.png" },
-        { title: "Someone to you", album: "Banners", date: "13 october", src:"/BANNERS - Someone To You.mp3",time: "3:11", id: 4, img: "/tsrep.png" },
+        { title: "Someone to you", album: "Banners", date: "13 october", src:"/BANNERS - Someone To You.mp3",time: "3:11", id: 4, img: "banners.jpg" },
         { title: "Love me like you do", album: "Single", date: "13 october", src:"Music/Ellie Goulding - Love Me Like You Do (Official Video).mp3",time: "3:11", id: 5, img: "/tsrep.png" },
         { title: "As it was", album: "Harry's House", date: "13 october", src:"Music/Harry Styles - As It Was (Lyrics).mp3",time: "3:11", id: 6, img: "/tsrep.png" },
         { title: "How to save a life", album: "How to Save a Life", date: "13 october",src:"Music/The Fray - How to Save a Life (Official Video).mp3", time: "3:11", id: 7, img: "/tsrep.png" },
