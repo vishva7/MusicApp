b2466c035bac86f27266616d993387abd46ec449
Vijju <vijjuhavaldar@gmail.com>
2023-11-24 11:32:43 +0530

    modified

--- src/routes/loginpage.js
+++ src/routes/loginpage.js
@@ -174,7 +174,7 @@ export default function LoginPage() {
                 type="submit"
                 className="text-white bg-blue-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-80 sm:w-auto px-10 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 onSubmit={handleLogin}
-                // onClick={handleClick}
+                onClick={handleClick}
               >
                 Log in
               </button>
